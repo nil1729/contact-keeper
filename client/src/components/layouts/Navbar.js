5dff201386a09d4693738dbf2bbc4204422e7e39
nil1729 <nilanjan172nsvian@gmail.com>
2020-05-28 09:25:30 +0530

    add all routes

---
+++ client/src/components/layouts/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Navbar = ({title, icon}) => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+Navbar.proptypes={
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+}
+ 
+export default Navbar
