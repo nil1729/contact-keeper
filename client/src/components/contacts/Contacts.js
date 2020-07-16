3583824bab88fae69a7996ce13878c563b8eef3b
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-07-16 09:21:11 +0530

    add Transition

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,8 @@
-import React, {useContext, useEffect} from 'react'
-
+import React, { useContext, useEffect } from 'react'
+import {
+    CSSTransition,
+    TransitionGroup,
+} from 'react-transition-group';
 // Context 
 import ContactContext from '../../context/contact/contactContext';
 
@@ -12,46 +15,54 @@ import Spinner from '../layouts/Spinner';
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
-    const {getContacts, contacts, filtered, loading} = contactContext;
-    useEffect(()=>{
+    const { getContacts, contacts, filtered, loading } = contactContext;
+    useEffect(() => {
         getContacts();
         // eslint-disable-next-line
     }, []);
-  return (
-    <div className="container p-3" style={{position: 'relative'}}>
-        <ContactFilter/>
-        {loading ? (
-            <Spinner/>
-            ): (
-                contacts.length === 0 ? (
-                <p className="lead">Please Add a Contact</p>
-            ):(
-        <div className="contacts-div container border border-secondary mb-4" style={{
-                position:'absolute',
-                height: '30rem',
-                'overflowY': 'scroll'
-            }}>
-            <ul className="list-group">
-            {
-                filtered !== null ? filtered.map(contact=>(
-                    <ContactItem
-                        key={contact._id}
-                        contact={contact}
-                    />
-                )):
-                contacts.map(contact=>(
-                    <ContactItem
-                        key={contact._id}
-                        contact={contact}
-                    />
-                ))
-            }
-            </ul>
+    return (
+        <div className="container p-3" style={{ position: 'relative' }}>
+            <ContactFilter />
+            {loading ? (
+                <Spinner />
+            ) : (
+                    contacts.length === 0 ? (
+                        <p className="lead">Please Add a Contact</p>
+                    ) : (
+                            <div className="contacts-div container border border-secondary mb-4" style={{
+                                position: 'absolute',
+                                height: '30rem',
+                                'overflowY': 'scroll'
+                            }}>
+
+                                <ul className="list-group">
+                                    <TransitionGroup >
+                                        {
+
+                                            filtered !== null ? filtered.map(contact => (
+                                                <CSSTransition timeout={500} classNames="my-item" key={contact._id}>
+                                                    <ContactItem
+                                                        contact={contact}
+                                                    />
+                                                </CSSTransition>
+
+                                            )) :
+                                                contacts.map(contact => (
+                                                    <CSSTransition timeout={500} classNames="my-item" key={contact._id}>
+                                                        <ContactItem
+                                                            contact={contact}
+                                                        />
+                                                    </CSSTransition>
+                                                ))
+                                        }
+                                    </TransitionGroup>
+                                </ul>
+
+                            </div>
+                        )
+                )}
         </div>
-            )
-            )}
-    </div>
-  )
+    )
 }
 
 export default Contacts
